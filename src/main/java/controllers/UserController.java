b39a9d0a8822918202b876982038f967cfe25ae9
giakoii <khoipgse172073@fpt.edu.vn>
2024-07-07 14:52:12 +0700

    cấu hình docker x3

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello User!";
+    }
+}
